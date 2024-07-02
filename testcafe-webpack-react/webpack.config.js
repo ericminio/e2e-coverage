9c7f23551aa131c87fbdfe98b9b5f97d88e259aa
Eric Mignot <eric.mignot@gmail.com>
2024-07-02 15:20:32 -0700

    faster build

--- testcafe-webpack-react/webpack.config.js
+++ testcafe-webpack-react/webpack.config.js
@@ -4,6 +4,8 @@ import path, { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+import webpack from 'webpack';
+
 export default {
     mode: 'development',
     entry: './.instrumented/client/index.tsx',
@@ -23,4 +25,9 @@ export default {
             },
         ],
     },
+    plugins: [
+        new webpack.ProvidePlugin({
+            React: 'react',
+        }),
+    ],
 };
